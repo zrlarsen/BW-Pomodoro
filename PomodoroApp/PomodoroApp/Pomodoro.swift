c5c4aad07072c76c272fb0b719817761109c8a5f
Zack Larsen <zacklarsen1@gmail.com>
2019-11-20 17:50:55 -0700

    did the files add

---
+++ PomodoroApp/PomodoroApp/Pomodoro.swift
@@ -0,0 +1,83 @@
+//
+//  Pomodoro.swift
+//  buildweek
+//
+//  Created by Zack Larsen on 11/18/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import Foundation
+
+protocol PomodoroDelegate: class {
+    func countdownDidUpdate(timeRemaining: TimeInterval)
+    func countdownDidFinish()
+    
+    
+}
+
+enum TimerState {
+    case started
+    case finished
+    case reset
+}
+
+class Countdown {
+    
+    weak var delegate: PomodoroDelegate?
+    var duration: TimeInterval
+    var timer: Timer?
+    var stopDate: Date?
+    var state: TimerState
+    
+    
+    var timeRemaining: TimeInterval {
+        if let stopDate = stopDate {
+            let timeRemaing = stopDate.timeIntervalSinceNow
+            return timeRemaing
+            } else {
+            return 0
+        }
+    }
+    init() {
+        timer = nil
+        stopDate = nil
+        duration = 1500
+        state = .finished
+    }
+    
+    func start() {
+        cancelTimer()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true, block: updateTimer(timer:))
+        stopDate = Date().addingTimeInterval(duration)
+        state = .started
+    }
+    
+    func updateTimer(timer: Timer) {
+        if let stopDate = stopDate {
+            let currentTime = Date()
+            
+            if currentTime <= stopDate {
+                
+                delegate?.countdownDidUpdate(timeRemaining: timeRemaining)
+            } else {
+                state = .finished
+                cancelTimer()
+                self.stopDate = nil
+                delegate?.countdownDidFinish()
+                
+            }
+        }
+    }
+    func reset() {
+        stopDate = nil
+        cancelTimer()
+        state = .reset
+    }
+
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
+
+
---
+++ PomodoroApp/PomodoroApp/PomodoroViewController.swift
@@ -0,0 +1,90 @@
+//
+//  PomodoroViewController.swift
+//  buildweek
+//
+//  Created by Zack Larsen on 11/18/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import UIKit
+
+class PomodoroViewController: UIViewController {
+
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var breakButton: UIButton!
+    
+    let countdown = Countdown()
+    
+    private var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+    
+    private var countdownLabel: [[String]] = {
+        
+        let minutes: [String] = Array(0...25).map({ String($0) })
+        let seconds: [String] = Array(0...59).map({ String($0) })
+        
+        return [minutes, ["min"], seconds, ["sec"]]
+    }()
+    
+//    func duration(from duration: TimeInterval) {
+//        let minutes =
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func updateViews() {
+        switch countdown.state {
+        case .started:
+            timerLabel.adjustsFontSizeToFitWidth = true
+            timerLabel.text = string(from:countdown.timeRemaining)
+        case.finished:
+            timerLabel.text = string(from: 0)
+            startButton.isEnabled = true
+        case .reset:
+            timerLabel.text = string(from: countdown.duration); startButton.isEnabled = false
+        }
+    }
+    func string(from duration: TimeInterval) -> String {
+        let date = Date(timeIntervalSinceReferenceDate: duration)
+        return dateFormatter.string(from: date)
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Pomodoro Finished!", message: "Start your break now.", preferredStyle: .alert)
+        
+        let breakAction = UIAlertAction(title: "Take a Break", style: .default, handler: nil)
+        
+        alert.addAction(breakAction)
+    }
+    @IBAction func startButton(_ sender: Any) {
+        countdown.start()
+        updateViews()
+    }
+    
+    @IBAction func breakButton(_ sender: Any) {
+        countdown.start()
+        updateViews()
+    }
+    
+}
+  
+extension PomodoroViewController: PomodoroDelegate {
+    func countdownDidUpdate(timeRemaining: TimeInterval) {
+        updateViews()
+    }
+    func countdownDidFinish() {
+        showAlert()
+        updateViews()
+    }
+}
+
